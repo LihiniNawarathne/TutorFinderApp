abc4929388529c2a57e3c5162cc4f0af6bd8dc34
NawarathneL <79820418+NawarathneL@users.noreply.github.com>
2021-03-21 12:55:13 +0530

    Completed StudenUI Layouts

---
+++ app/src/main/java/com/example/tutorfinder/StudentUI/selectedOfferStudent.java
@@ -0,0 +1,16 @@
+package com.example.tutorfinder.StudentUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tutorfinder.R;
+
+public class selectedOfferStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_offer_student);
+    }
+}
