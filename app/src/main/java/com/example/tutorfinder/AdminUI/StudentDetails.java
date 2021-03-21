625d69c869923150ffcd2c630167530f8d3ec6da
PabasaraPalihena <pabapalihena@gmail.com>
2021-03-21 22:35:04 +0530

    Add admin Layout designs

---
+++ app/src/main/java/com/example/tutorfinder/AdminUI/StudentDetails.java
@@ -0,0 +1,14 @@
+package com.example.tutorfinder.AdminUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tutorfinder.R;
+public class StudentDetails extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_details);
+    }
+}
