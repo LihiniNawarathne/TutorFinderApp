dabd61c93a3ba3fd9444296aa12fe81bfd969d6b
KaveethaBandaraAramba <70481839+KaveethaBandaraAramba@users.noreply.github.com>
2021-03-22 08:14:54 +0530

    addingtutor

---
+++ app/src/main/java/com/example/tutorfinder/TutorUI/TutorProfile.java
@@ -0,0 +1,16 @@
+package com.example.tutorfinder.TutorUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tutorfinder.R;
+
+public class TutorProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_profile);
+    }
+}
