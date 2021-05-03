623e7608eff3eb557bd5376a6fb7cec3816ee1b4
NawarathneL <79820418+NawarathneL@users.noreply.github.com>
2021-05-04 00:10:20 +0530

    Created Notification fragment.

--- app/src/main/java/com/example/tutorfinder/StudentUI/CourseOffersStudent.java
+++ app/src/main/java/com/example/tutorfinder/StudentUI/CourseOffersStudent.java
@@ -21,5 +21,73 @@ public class CourseOffersStudent extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_offers_student);
 
+        //set action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Search Results");
+
+        //enable back button
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        offerbtnMaths =findViewById(R.id.btnMaths);
+        offerbtnBio =findViewById(R.id.btnBio);
+        offerbtnCommerce =findViewById(R.id.btnCommerce);
+        offerbtnArt =findViewById(R.id.btnArt);
+
+        //Science(Maths) btn clicked
+        offerbtnMaths.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Stream = "Science(Maths)";
+
+                intent = new Intent(CourseOffersStudent.this, selectedOfferStudent.class);
+                intent.putExtra("subject ", Stream );
+                startActivity(intent);
+            }
+        });
+
+        //Science(Bio) btn clicked
+        offerbtnBio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Stream = "Science(Bio)";
+
+                intent = new Intent(CourseOffersStudent.this, selectedOfferStudent.class);
+                intent.putExtra("subject ", Stream );
+                startActivity(intent);
+            }
+        });
+
+        //Commerce btn clicked
+        offerbtnCommerce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Stream = "Commerce";
+
+                intent = new Intent(CourseOffersStudent.this, selectedOfferStudent.class);
+                intent.putExtra("subject ", Stream );
+                startActivity(intent);
+            }
+        });
+
+        //Commerce btn clicked
+        offerbtnArt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Stream = "Art";
+
+                intent = new Intent(CourseOffersStudent.this, selectedOfferStudent.class);
+                intent.putExtra("subject ", Stream );
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        //navigate to previous activity
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
