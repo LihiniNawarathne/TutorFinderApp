698ca2bfe30bb2eb7750fee5d299b472ad893524
PabasaraPalihena <pabapalihena@gmail.com>
2021-05-05 20:43:22 +0530

    retrieve payment details of the students to recyclerview retrieve bankslip form the database create student payment model and student payment adapter

---
+++ app/src/main/java/com/example/tutorfinder/AdminUI/admin_add_students_to_chat_groups.java
@@ -0,0 +1,61 @@
+package com.example.tutorfinder.AdminUI;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.tutorfinder.Admin_adapters.studentpaymentAdapter;
+import com.example.tutorfinder.Admin_models.studentPayment;
+import com.example.tutorfinder.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class admin_add_students_to_chat_groups extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root = db.getReference("joinGroupClass");
+    // Button add,delete;
+
+    private studentpaymentAdapter adapter;
+    private ArrayList<studentPayment> stupaymentList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_add_students_to_chat_groups);
+
+        recyclerView = findViewById(R.id.rvpaymnets);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        stupaymentList = new ArrayList<>();
+
+        root.addValueEventListener(new ValueEventListener() {
+            @Override
+
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                stupaymentList.clear();
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    studentPayment studentPay = dataSnapshot.getValue(studentPayment.class);
+                    stupaymentList.add(studentPay);
+                    adapter = new studentpaymentAdapter(stupaymentList, admin_add_students_to_chat_groups.this);
+                    recyclerView.setAdapter(adapter);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
